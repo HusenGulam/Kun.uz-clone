01fe49d997880aad65da70642fa11a38fe27065b
HusenGulam <hsencodung77@gmail.com>
2023-12-27 21:46:21 -0800

    Kun.uz clone started

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Ishni boshlanglar OMAD 😊
         </p>
         <a
           className="App-link"
